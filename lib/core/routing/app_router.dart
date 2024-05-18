d5bf3799cf1499e840e78ef39a8e8d48d58de0f4
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-18 16:25:02 +0300

    chore: Add AppRouter and OnBoardingScreen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:doctor_system/core/routing/routes.dart';
+import 'package:doctor_system/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const Placeholder());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
