78c489a4b6d782fb07cec15590b6a08d90b13edb
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-22 14:32:03 +0300

    feat: Add launch configurations for development and production environments

---
+++ lib/main_development.dart
@@ -0,0 +1,14 @@
+import 'package:doctor_system/core/di/dependency_injection.dart';
+import 'package:doctor_system/core/routing/app_router.dart';
+import 'package:doctor_system/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  setupGetIt();
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,9 +2,11 @@ import 'package:doctor_system/core/di/dependency_injection.dart';
 import 'package:doctor_system/core/routing/app_router.dart';
 import 'package:doctor_system/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
