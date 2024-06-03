22e565dd5462ffc9a9201279f0904a1db772d93a
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-03 21:36:54 +0300

    feat: Add SVG notification icon to home top bar

--- lib/features/home/ui/widgets/home_top_bar.dart
+++ lib/features/home/ui/widgets/home_top_bar.dart
@@ -1,5 +1,7 @@
+import 'package:doctor_system/core/theming/app_colors.dart';
 import 'package:doctor_system/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeTopBar extends StatelessWidget {
   const HomeTopBar({super.key});
@@ -14,9 +16,21 @@ class HomeTopBar extends StatelessWidget {
             Text(
               'Hi, Hashim!',
               style: TextStyles.font18DarkBlueBold,
+            ),
+            Text(
+              'How are you today?',
+              style: TextStyles.font12GrayRegular,
             )
           ],
-        )
+        ),
+        const Spacer(),
+        CircleAvatar(
+          radius: 24.0,
+          backgroundColor: AppColors.moreLighterGray,
+          child: SvgPicture.asset(
+            'assets/svgs/notifications.svg',
+          ),
+        ),
       ],
     );
   }
