1b04f8e59255044f4429d187163bac018c5eaacd
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-07 19:19:55 +0300

    feat: Add flutter_secure_storage and system token to secure token

--- lib/features/login/logic/cubit/login_cubit.dart
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -33,7 +33,7 @@ class LoginCubit extends Cubit<LoginState> {
   }
 
   Future<void> saveUsertoken(String token) async {
-    await SharedPrefHelper.setData(SharedPrefKeys.userToken, token);
+    await SharedPrefHelper.setSecuredString(SharedPrefKeys.userToken, token);
     DioFactory.setTokenIntoHeaderAfterLogin(token);
   }
 }
